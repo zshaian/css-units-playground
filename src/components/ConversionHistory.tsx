cbef176d758e78b516b0b8a44a4bc66d9a4fce7c
shaian <zshaian@users.noreply.github.com>
2024-12-07 15:20:15 +0800

    chore: adjust icon size and alignments of elements

--- src/components/ConversionHistory.tsx
+++ src/components/ConversionHistory.tsx
@@ -20,19 +20,19 @@ interface ConversionHistoryProps {
 
 const ConversionHistory: React.FC<ConversionHistoryProps> = (props) => {
   return (
-    <div className="flex flex-col gap-y-3">
-      <span className="px-4 py-2 font-light border border-foreground uppercase">
+    <div className="w-max flex flex-col gap-y-3">
+      <span className="w-max px-4 py-1 font-light border border-foreground rounded-lg uppercase">
         History
       </span>
       <section className="flex flex-col gap-y-4">
         {props.conversionHistoryList.map((item) => (
           <div className="flex flex-col gap-y-3">
-            <div className="flex justify-between">
+            <div className="flex items-center justify-between gap-x-3">
               <ConversionItem unit={item.fromUnit} value={item.from} />
-              <MoveRight size={40} />
+              <MoveRight size={30} />
               <ConversionItem unit={item.toUnit} value={item.to} />
             </div>
-            <p className="text-muted font-light">
+            <p className="text-foreground font-light">
               Root Font Size:{" "}
               <span className="font-bold">{item.rootFontSize}</span>
             </p>
@@ -45,11 +45,11 @@ const ConversionHistory: React.FC<ConversionHistoryProps> = (props) => {
 
 const ConversionItem: React.FC<ConversionItemProps> = (props) => {
   return (
-    <div className="px-4 py-2 flex gap-x-2 border border-foreground">
+    <div className="p-2 flex items-center gap-x-2 border border-foreground rounded-md">
       <span className="font-bold uppercase">{props.value}</span>
       <span className="font-bold uppercase">{props.unit}</span>
       <Button variant="outline">
-        <Copy size={30} />
+        <Copy size={12} />
       </Button>
     </div>
   );
