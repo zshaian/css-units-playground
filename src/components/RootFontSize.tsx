59d8b850e35a9d8c58cfa768415c17b3142ab462
shaian <zshaian@users.noreply.github.com>
2024-12-07 15:32:37 +0800

    feat: add tooltip about the root font-size

--- src/components/RootFontSize.tsx
+++ src/components/RootFontSize.tsx
@@ -1,19 +1,41 @@
 import { Info } from "lucide-react";
 import { Input } from "./ui/input";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from "./ui/tooltip";
+import { Button } from "./ui/button";
 
 const RootFontSize: React.FC = () => {
   return (
-    <p className="flex items-center gap-x-3">
-      <Info size={16} />
+    <p className="flex items-center gap-x-2">
+      <RootFontSizeTip />
       <span className="text-nowrap">Root Font Size</span>
-      <Input
-        className="w-[5.5ch] "
-        type="number"
-        defaultValue={16}
-      />
+      <Input className="w-[5.5ch] " type="number" defaultValue={16} />
       <span>Pixel</span>
     </p>
   );
 };
+
+const RootFontSizeTip: React.FC = () => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="ghost">
+          <Info size={16} />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent className="px-4 py-2 bg-foreground text-background rounded-md">
+        <p>
+          Set the base font size (
+          <span className="font-bold">default: 16px</span>). This value is used
+          to calculate relative CSS units like REM and EM during conversions.
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
 
 export default RootFontSize;
