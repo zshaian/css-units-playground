fee7dee4283987ea0711c0271b87db12855ad67c
shaian <zshaian@users.noreply.github.com>
2024-12-09 12:05:47 +0800

    chore: adjust spacing and set a static width to the input element

--- src/components/UnitInput.tsx
+++ src/components/UnitInput.tsx
@@ -4,8 +4,8 @@ import { Input } from "./ui/input";
 
 const UnitInput: React.FC = () => {
   return (
-    <div className="flex gap-x-2 border border-input rounded-md">
-      <Input type="number" className="no-spinner border-none" />
+    <div className="flex gap-x-1 border border-input rounded-md">
+      <Input type="number" className="no-spinner w-[15rem] border-none" />
       <Button variant="ghost">
         <Copy size={30} />
       </Button>
