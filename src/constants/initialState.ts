01268677de109f70e34d072cee60b5bb8926c25f
shaian <zshaian@users.noreply.github.com>
2024-12-11 21:35:22 +0800

    feat: create the initial state for the app

---
+++ src/constants/initialState.ts
@@ -0,0 +1,12 @@
+import type { UnitState } from "@/types";
+
+const INITIAL_UNIT_STATE_VALUE: UnitState = {
+  fromUnitValue: 16,
+  fromUnit: "px",
+  toUnitValue: 1,
+  toUnit: "rem",
+  rootFontSize: 16,
+  baseFontSize: 16,
+};
+
+export { INITIAL_UNIT_STATE_VALUE };
