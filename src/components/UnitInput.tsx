c75c48b4e9b9d15e160afdd3a06f2631653b2cab
shaian <zshaian@users.noreply.github.com>
2024-12-11 21:34:49 +0800

    feat: add the property from the unitInput component

--- src/components/UnitInput.tsx
+++ src/components/UnitInput.tsx
@@ -2,15 +2,25 @@ import { Copy } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
-const UnitInput: React.FC = () => {
-  return (
+interface UnitInputProps {
+  name:string;
+  unitValue:string | number;  
+  handleUnitValueChange:(event:React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const UnitInput:React.FC<UnitInputProps> = ({name,unitValue,handleUnitValueChange}) => (
     <div className="flex gap-x-1 border border-input rounded-md">
-      <Input type="number" className="no-spinner w-[15rem] border-none" />
+      <Input
+        type="number"
+        name={name}
+        value={(unitValue === 0) ? "" : unitValue}
+        onChange={handleUnitValueChange}
+        className="no-spinner w-[15rem] border-none"
+      />
       <Button variant="ghost">
         <Copy size={30} />
       </Button>
     </div>
-  );
-};
+);
 
 export default UnitInput;
