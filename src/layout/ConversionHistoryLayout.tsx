24e3fe0593c7f06d57c50f2715af187ace5cd861
shaian <zshaian@users.noreply.github.com>
2024-12-11 22:08:12 +0800

    refactor: remove all of the components in the ConversionHistoryLayout component
    all of the components is just going to move inside when the component is
    being used, to match its purpose as a layout component

--- src/layout/ConversionHistoryLayout.tsx
+++ src/layout/ConversionHistoryLayout.tsx
@@ -1,96 +1,22 @@
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
-
-import { useMediaQuery } from "@/hooks/useMediaQuery";
-
-import ConversionHistory from "@/components/ConversionHistory";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-const ConversionHistoryLayout: React.FC = () => {
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
+interface ConversionHistoryLayoutProps {
+  children: React.ReactNode;
+}
+
+const ConversionHistoryLayout: React.FC<ConversionHistoryLayoutProps> = (
+  props
+) => {
   const isDesktop = useMediaQuery("(min-width:768px)");
-  const dummyConversionHistoryList = [
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-    {
-      from: 5,
-      fromUnit: "rem",
-      to: 80,
-      toUnit: "px",
-      rootFontSize: "16px",
-      baseFontSize: "16px",
-    },
-  ];
 
   if (isDesktop) {
     return (
       <ScrollArea className="h-[100vh] border-r border-foreground">
-        <section>
-          <ConversionHistory
-            conversionHistoryList={dummyConversionHistoryList}
-          />
-        </section>
+        <section>{props.children}</section>
       </ScrollArea>
     );
   }
@@ -102,9 +28,7 @@ const ConversionHistoryLayout: React.FC = () => {
           History
         </Button>
       </DrawerTrigger>
-      <DrawerContent>
-        <ConversionHistory conversionHistoryList={dummyConversionHistoryList} />
-      </DrawerContent>
+      <DrawerContent>{props.children}</DrawerContent>
     </Drawer>
   );
 };
