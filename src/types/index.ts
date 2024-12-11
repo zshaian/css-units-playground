f88e6cd790a6b0c277b70c677042b87895d3d880
shaian <zshaian@users.noreply.github.com>
2024-12-11 21:36:33 +0800

    refactor: change the name of the types from store to state

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,23 @@
-type ConversionDirection = "from-unit" | "to-unit";
-
 type ValidUnits = "rem" | "em" | "%" | "px";
 
-interface UnitStoreState {
+interface UnitState {
   fromUnitValue: number;
   fromUnit: ValidUnits;
-  toUnitValue: number;
+  toUnitValue: number; 
   toUnit: ValidUnits;
-  rootFontSize: number;
+  rootFontSize: number; 
   baseFontSize: number;
-  changeFromUnitValue: (newUnitValue: number | string) => void;
-  changeFromUnit: (newUnit: ValidUnits) => void;
-  changeToUnitValue: (newUnitValue: number | string) => void;
-  changeToUnit: (newUnit: ValidUnits) => void;
-  changeRootFontSize: (newRootFontSize: number) => void;
-  changeBaseFontSize: (newBaseFontSize: number) => void;
 }
 
-export type { UnitStoreState, ConversionDirection, ValidUnits };
+interface ConvertionInfoValue {
+  fromUnit: ValidUnits;
+  toUnit: ValidUnits;
+  formula: string;
+  calculation: string;
+}
+
+type NoConvertionKeys = `rem->rem` | `em->em` | `px->px` | `%->%`;
+type ConvertionInfoKeys = Exclude<`${ValidUnits}->${ValidUnits}`,NoConvertionKeys>; 
+type ConvertionInfoProps = Record<ConvertionInfoKeys,ConvertionInfoValue>;
+
+export type { UnitState,ConvertionInfoProps,ConvertionInfoValue, ValidUnits };
