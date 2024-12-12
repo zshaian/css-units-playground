2b4d62eedbb8c26befe4ee3deb2d64abb952dea1
shaian <zshaian@users.noreply.github.com>
2024-12-12 13:05:04 +0800

    chore: add the export next to the variable

--- src/constants/initialState.ts
+++ src/constants/initialState.ts
@@ -1,6 +1,6 @@
 import type { UnitState } from "@/types";
 
-const INITIAL_UNIT_STATE_VALUE: UnitState = {
+export const INITIAL_UNIT_STATE_VALUE: UnitState = {
   fromUnitValue: 16,
   fromUnit: "px",
   toUnitValue: 1,
@@ -8,5 +8,3 @@ const INITIAL_UNIT_STATE_VALUE: UnitState = {
   rootFontSize: 16,
   baseFontSize: 16,
 };
-
-export { INITIAL_UNIT_STATE_VALUE };
