3a849f347320c4aab73337d62b7f3645fc603ce2
shaian <zshaian@users.noreply.github.com>
2024-12-12 09:43:19 +0800

    feat: add the click event to the save history button

--- src/components/SaveToHistoryButton.tsx
+++ src/components/SaveToHistoryButton.tsx
@@ -1,8 +1,12 @@
 import { Button } from "./ui/button";
 
-const SaveToHistoryButton: React.FC = () => {
+interface SaveToHistoryButtonProps {
+ handleSaveToHistoryEvent:() => void;
+}
+
+const SaveToHistoryButton: React.FC<SaveToHistoryButtonProps> = (props) => {
   return (
-    <Button className="px-4 py-2 capitalize" variant="outline">
+    <Button className="px-4 py-2 capitalize" variant="outline" onClick={props.handleSaveToHistoryEvent}>
       save to history
     </Button>
   );
